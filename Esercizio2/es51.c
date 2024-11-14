91c1c17eb041a6a5bed56e2497684797cf13d6c5
Arjel Buzi <arjel.buzi1@gmail.com>
2024-11-14 14:56:41 +0100

    test 1.1

--- Esercizio2/es51.c
+++ Esercizio2/es51.c
@@ -4,17 +4,63 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdlib.h>
+
+int isVowel(char c) {
+    c = (c >= 'A' && c <= 'Z') ? c + ('a' - 'A') : c; 
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
 
 
 int main(void){
+    char buffer [1024];
+    char *p;
+
+    long int rb;
     
+    pid_t pd1;
+    pid_t pd2;
+
+    int fd2,fd3;
+
+    int fd = open("file.txt",O_RDWR);
+    if(fd<0)
+        exit(-1);
+
+    
+
     //variabili per i due processi
-    pid_t p1 = fork();
-    pid_t p2;
+    if(fork()>0){
+        if(fork()==0){
+            pd2 = getpid();    // Processo figlio 2
+            printf("Questo è il processo figlio 2\n");
+            fd2 =open("consonanti.txt",O_CREAT|O_RDWR,0644);
+            while(rb = read(fd,buffer,1)){
+                p = buffer;
+                while(*p++){
+                    if(!isVowel(*p))
+                        write(fd2,p,1);
+            }    
+            }       
+        }
+        
+    }
+    else{
+        pd1 = getpid();         // Processo figlio 1
+        printf("Questo è il processo figlio 1\n");
+        fd2 =open("vocali.txt",O_CREAT|O_RDWR,0644);
+        while(rb = read(fd,buffer,1)){
+            p = buffer;
+            while(*p++){
+                if(isVowel(*p))
+                    write(fd2,p,1);
+            }
+        } 
+    }
 
-    
-    
 
-    if (p1 > 0){
-        p2 = fork();}
+
+    exit(0);
+    
 }
