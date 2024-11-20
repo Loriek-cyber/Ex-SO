269d78d0bb043702f537ad5da77c0b74d8f1596a
Arjel Buzi <arjel.buzi1@gmail.com>
2024-11-20 13:50:05 +0100

    inizializato il codice in eser4

--- Esercizio4/main.c
+++ Esercizio4/main.c
@@ -1,1 +1,25 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BSize 1024
+
+int strlen(char *str) {
+    int len = 0;
+    while (*str != '\0') {
+        len++;}
+    return len;
+}
+
+void println(char *str) {
+    write(STDOUT_FILENO,str,strlen(str));
+    write(STDOUT_FILENO,"\n",1);
+}
+
+
+int main(int argc, char *argv[]) {
+    
+
+}
